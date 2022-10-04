481226e6699805bcd7bab951396f5cd5712081f7
Willem Boshoff <boshoffwillem@protonmail.com>
2022-10-04 14:56:30 +0200

    Clean up install.sh

--- install.sh
+++ install.sh
@@ -7,20 +7,20 @@ sh <(curl -L https://nixos.org/nix/install) --no-daemon
 # install packages
 
 nix-env -iA \
-	nixpkgs.starship \
-	nixpkgs.git \
-	nixpkgs.delta \
-	nixpkgs.rustup \
-	nixpkgs.gcc \
-	nixpkgs.stow \
-	nixpkgs.nodejs \
+  nixpkgs.starship \
+  nixpkgs.git \
+  nixpkgs.delta \
+  nixpkgs.rustup \
+  nixpkgs.gcc \
+  nixpkgs.stow \
+  nixpkgs.nodejs \
   nixpkgs.nodePackages.cspell \
   nixpkgs.dotnet-sdk \
-	nixpkgs.neovim \
-	nixpkgs.ispell \
-	nixpkgs.tmux \
-	nixpkgs.ripgrep \
-	nixpkgs.exa \
-	nixpkgs.bat \
+  nixpkgs.neovim \
+  nixpkgs.ispell \
+  nixpkgs.tmux \
+  nixpkgs.ripgrep \
+  nixpkgs.exa \
+  nixpkgs.bat \
   nixpkgs.powershell \
   nixpkgs.emacs
