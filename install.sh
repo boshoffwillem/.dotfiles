dcd0acd8f5818d7d34b387ec338c3761e24c15e1
Willem Boshoff <boshoffwillem@protonmail.com>
2022-12-22 15:41:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,28 +24,14 @@ nix-env -iA \
   nixpkgs.bat \
   nixpkgs.powershell \
   nixpkgs.tree
+  
+# use starship shell prompt
+echo '' | sudo tee -a ~/.profile
+echo 'alias ls="exa"' | sudo tee -a ~/.profile
+echo 'alias cat="bat"' | sudo tee -a ~/.profile
+echo '# use starship prompt' | sudo tee -a ~/.profile
+echo 'eval "$(starship init bash)"' | sudo tee -a ~/.profile
 
-#sudo zypper install -y starship
-#sudo zypper install -y delta
-#sudo zypper install -y gcc
-#sudo zypper install -y gcc-c++
-#sudo zypper install -y clang
-#sudo zypper install -y stow
-#sudo zypper install -y neovim
-#sudo zypper install -y emacs
-#sudo zypper install -y ripgrep
-#sudo zypper install -y exa
-#sudo zypper install -y bat
-#sudo zypper install -y tree
-#sudo zypper install -y tmux
-#sudo zypper install -y nodejs
-#sudo zypper install -y npm
-
-#sudo zypper install -y libicu
-#sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-#wget https://packages.microsoft.com/config/opensuse/15/prod.repo
-#sudo mv prod.repo /etc/zypp/repos.d/microsoft-prod.repo
-#sudo chown root:root /etc/zypp/repos.d/microsoft-prod.repo
-#sudo zypper install -y dotnet-sdk-6.0
-
-#sudo dotnet tool install --global powershell
+# nvim config
+git clone https://github.com/boshoffwillem/nvim\
+    ~/.config/nvim
