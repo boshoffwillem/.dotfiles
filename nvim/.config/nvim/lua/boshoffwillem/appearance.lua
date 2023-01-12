9f5b3ba94b1589381d10301a87c04ee40bbf850b
willem.boshoff <willem.boshoff@dyna-mo.com>
2023-01-12 10:31:34 +0200

    Update theme

--- nvim/.config/nvim/lua/boshoffwillem/appearance.lua
+++ nvim/.config/nvim/lua/boshoffwillem/appearance.lua
@@ -1,8 +1,30 @@
 -- vim.cmd [[colorscheme onedark]]
 
-vim.g.vscode_italic_comment = 1
-vim.g.vscode_disable_nvimtree_bg = true
-vim.cmd([[colorscheme vscode]])
+-- vim.cmd([[colorscheme vscode]])
+vim.o.background = 'light'
+local c = require('vscode.colors').get_colors()
+require('vscode').setup({
+    -- Enable transparent background
+    transparent = true,
+
+    -- Enable italic comment
+    italic_comments = true,
+
+    -- Disable nvim-tree background color
+    disable_nvimtree_bg = true,
+
+    -- Override colors (see ./lua/vscode/colors.lua)
+    color_overrides = {
+        vscLineNumber = '#FFFFFF',
+    },
+
+    -- Override highlight groups (see ./lua/vscode/theme.lua)
+    group_overrides = {
+        -- this supports the same val table as vim.api.nvim_set_hl
+        -- use colors from this colorscheme by requiring vscode.colors!
+        Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+    }
+})
 
 -- Set lualine as statusline
 -- See `:help lualine.txt`
