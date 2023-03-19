9b42e2deb7fbc14f3e9253d546ea5f2291a198cc
Willem Boshoff <boshoffwillem@protonmail.com>
2023-03-19 19:39:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,38 +1,39 @@
-# install nix
-sh <(curl -L https://nixos.org/nix/install) --no-daemon
-
-# source nix
-. ~/.nix-profile/etc/profile.d/nix.sh
 
 # install packages
 
-nix-env -iA \
-  nixpkgs.starship \
-  nixpkgs.git \
-  nixpkgs.delta \
-  nixpkgs.rustup \
-  nixpkgs.gcc \
-  nixpkgs.gnumake \
-  nixpkgs.stow \
-  nixpkgs.nodejs \
-  nixpkgs.nodePackages.cspell \
-  nixpkgs.dotnet-sdk \
-  nixpkgs.neovim \
-  nixpkgs.ispell \
-  nixpkgs.tmux \
-  nixpkgs.ripgrep \
-  nixpkgs.exa \
-  nixpkgs.bat \
-  nixpkgs.powershell \
-  nixpkgs.tree
-  
+sudo apt install git \
+    gcc \
+    starship \
+    stow \
+    delta \
+    neovim \
+    bat \
+    exa \
+    tree \
+    -y
+    
 # use starship shell prompt
-echo '' | sudo tee -a ~/.profile
-echo 'alias ls="exa"' | sudo tee -a ~/.profile
-echo 'alias cat="bat"' | sudo tee -a ~/.profile
-echo '# use starship prompt' | sudo tee -a ~/.profile
-echo 'eval "$(starship init bash)"' | sudo tee -a ~/.profile
+echo 'alias ls="exa"' | sudo tee -a ~/.bashrc
+echo 'alias cat="bat"' | sudo tee -a ~/.bashrc
+echo '# use starship prompt' | sudo tee -a ~/.bashrc
+echo 'eval "$(starship init bash)"' | sudo tee -a ~/.bashrc
 
-# nvim config
-git clone git@github.com:boshoffwillem/nvim.git \
-  ~/.config/nvim
+#nix-env -iA \
+#  nixpkgs.starship \
+#  nixpkgs.git \
+#  nixpkgs.delta \
+#  nixpkgs.rustup \
+#  nixpkgs.gcc \
+#  nixpkgs.gnumake \
+#  nixpkgs.stow \
+#  nixpkgs.nodejs \
+#  nixpkgs.nodePackages.cspell \
+#  nixpkgs.dotnet-sdk \
+#  nixpkgs.neovim \
+#  nixpkgs.ispell \
+#  nixpkgs.tmux \
+#  nixpkgs.ripgrep \
+#  nixpkgs.exa \
+#  nixpkgs.bat \
+#  nixpkgs.powershell \
+#  nixpkgs.tree
