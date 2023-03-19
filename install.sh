b54d50edf2908c7ab085793031c1c2e2a70d9db7
Willem Boshoff <willem.boshoff@dyna-mo.com>
2023-03-19 20:40:54 +0200

    Update install script for Ubuntu

--- install.sh
+++ install.sh
@@ -1,39 +1,37 @@
 
 # install packages
 
-sudo apt install git \
-    gcc \
-    starship \
-    stow \
-    delta \
-    neovim \
-    bat \
-    exa \
-    tree \
-    -y
+sudo apt install git -y
+sudo apt install gcc -y
+#    starship \
+sudo apt install stow -y
+sudo apt install delta -y
+sudo apt install neovim -y
+sudo apt install bat -y
+sudo apt install exa -y
+sudo apt install ripgrep -y
+sudo apt install tree -y
+sudo apt install nodejs -y
+sudo apt install fzf -y
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     
 # use starship shell prompt
+curl -sS https://starship.rs/install.sh | sh
 echo 'alias ls="exa"' | sudo tee -a ~/.bashrc
 echo 'alias cat="bat"' | sudo tee -a ~/.bashrc
 echo '# use starship prompt' | sudo tee -a ~/.bashrc
 echo 'eval "$(starship init bash)"' | sudo tee -a ~/.bashrc
 
+stow starship
+stow nvim
+
 #nix-env -iA \
 #  nixpkgs.starship \
-#  nixpkgs.git \
-#  nixpkgs.delta \
 #  nixpkgs.rustup \
-#  nixpkgs.gcc \
 #  nixpkgs.gnumake \
-#  nixpkgs.stow \
 #  nixpkgs.nodejs \
 #  nixpkgs.nodePackages.cspell \
 #  nixpkgs.dotnet-sdk \
-#  nixpkgs.neovim \
 #  nixpkgs.ispell \
 #  nixpkgs.tmux \
-#  nixpkgs.ripgrep \
-#  nixpkgs.exa \
-#  nixpkgs.bat \
-#  nixpkgs.powershell \
-#  nixpkgs.tree
