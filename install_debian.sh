daed76d3da5e78db4a74287bc5ee6572796c5d71
Willem Boshoff <willem.boshoff@dyna-mo.com>
2025-03-19 21:00:23 +0200

    update install scripts

--- install_debian.sh
+++ install_debian.sh
@@ -46,6 +46,12 @@ sudo npm install -g @bufbuild/buf
 sudo npm install -g prettier
 sudo apt install shfmt -y
 
+# Dotnet
+# =============================================================================
+sudo add-apt-repository ppa:dotnet/backports
+sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-9.0
+# =============================================================================
 
 # Docker
 # =============================================================================
--- install_fedora.sh
+++ install_fedora.sh
@@ -24,6 +24,22 @@ sudo dnf install -y hyperfine
 sudo dnf install -y snapd
 sudo dnf install -y git-delta
 
+# Dotnet
+# =============================================================================
+sudo dnf install -y dotnet-sdk-9.0
+# =============================================================================
+
+# Docker
+# =============================================================================
+sudo dnf install -y gnome-terminal
+sudo dnf -y install dnf-plugins-core
+sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable --now docker
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-x86_64.rpm
+sudo dnf install ./docker-desktop-x86_64.rpm
+# =============================================================================
+
 # Rust
 # =============================================================================
 # Install rustup
