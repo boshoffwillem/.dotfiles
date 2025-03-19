30e7661aad01e3aa9989c84a26ba25976baa8a97
Willem Boshoff <willem.boshoff@dyna-mo.com>
2025-03-19 18:15:39 +0200

    add docker to debian script

--- install_debian.sh
+++ install_debian.sh
@@ -46,6 +46,29 @@ sudo npm install -g @bufbuild/buf
 sudo npm install -g prettier
 sudo apt install shfmt -y
 
+
+# Docker
+# =============================================================================
+# Add Docker's official GPG key:
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y gnome-terminal
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
+sudo apt-get install ./docker-desktop-amd64.deb
+sudo groupadd docker
+sudo usermod -aG docker $USER
+# =============================================================================
+
 # Rust
 # =============================================================================
 # Install rustup
