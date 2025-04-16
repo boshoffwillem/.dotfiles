ce4f623dc2f18921ce9043256bf9d0e663bc471e
Willem Boshoff <willem.boshoff@dyna-mo.com>
2025-04-16 08:04:07 +0200

    update gitignore

---
+++ emacs/.emacs.d/modules/project-management.el
@@ -0,0 +1,19 @@
+;;; project-management.el --- project management for emacs -*- lexical-binding: t -*-
+;; Author: Willem Boshoff <boshoffwillem@protonmail.com>
+;; URL: https://github.com/boshoffwillem/.emacs.d
+
+;;; Commentary:
+;; This file contains project management configuration
+
+;;; Code:
+(use-package projectile
+  :config
+  (projectile-mode +1)
+  ;; Recommended keymap prefix on macOS
+  ;; (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  ;; Recommended keymap prefix on Windows/Linux
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
+(provide 'project-management)
+
+;;; project-management.el ends here
