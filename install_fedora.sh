f4f53bbd0eac2ac3d92143037700ab42c5a63cd3
Willem Boshoff <boshoffwillem@protonmail.com>
2025-07-11 21:33:08 +0200

    update fedora script

--- install_fedora.sh
+++ install_fedora.sh
@@ -23,8 +23,53 @@ code=''
 docker=''
 pwsh=''
 
+sudo dnf update -y
+sudo dnf upgrade -y
+sudo dnf install -y stow
+
 git config --global user.email "boshoffwillem@protonmail.com"
 git config --global user.name "Willem Boshoff"
+
+# Rust
+# =============================================================================
+# Install rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# =============================================================================
+
+# use starship shell prompt
+# =============================================================================
+curl -sS https://starship.rs/install.sh | sh
+stow starship
+# =============================================================================
+
+if [ -d "~/jetbrains" ]; then
+    echo "Installing jetbrains toolbox..."
+    wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.7.0.48109.tar.gz
+    sudo tar -xvzf jetbrains-toolbox-2.7.0.48109.tar.gz
+    sudo mv jetbrains-toolbox-2.7.0.48109 ~/jetbrains
+    rm jetbrains-toolbox-2.7.0.48109.tar.gz
+else
+    echo "Skipping jetbrains toolbox installation."
+fi
+
+if [ -d "~/.dotnet" ]; then
+    echo "Installing dotnet..."
+    wget https://dot.net/v1/dotnet-install.sh -O ~/dotnet-install.sh
+    chmod +x ~/dotnet-install.sh
+    ~/dotnet-install.sh -c STS
+else
+    echo "Skipping dotnet installation."
+fi
+
+if [ -n "$terra" ] || [ -n "$all" ]; then
+    echo "Installing terraform..."
+else
+    echo "Skipping terraform installation."
+fi
+
+rm ~/.bashrc
+stow bashrc
+source ~/.bashrc
 
 # Get the options
 while getopts ":hn:" option; do
