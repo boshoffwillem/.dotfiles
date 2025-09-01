4ca46628b1669cb223e5de0c8251859e9d433644
Willem Boshoff <boshoffwillem@protonmail.com>
2025-09-01 22:48:48 +0200

    doom config

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -86,7 +86,6 @@
 ;; (set-default-coding-systems 'utf-8)
 ;; (set-language-environment 'utf-8)
 ;; (set-selection-coding-system 'utf-8)
-;; (setq visible-bell 1)  ; turn off beeps, make them flash!
 ;; (setq large-file-warning-threshold 100000000) ;; change to ~100 MB
 ;; (setq org-src-preserve-indentation t)
 ;;
@@ -95,24 +94,6 @@
 ;;       create-lockfiles nil)
 ;;
 ;; (setq custom-safe-themes t)
-;;
-;; (setq ring-bell-function 'ignore)
-
-;; Always scroll.
-;; (setq compilation-scroll-output t)
-;; Keyboard scroll one line at a time.
-;; (setq scroll-step 8)
-;; (setq auto-window-vscroll nil)
-;; (setq fast-but-imprecise-scrolling t)
-;; (setq scroll-conservatively 101)
-;; (setq scroll-margin 0)
-;; (setq scroll-preserve-screen-position t)
-
-;; Mouse scrolling
-;; (setq mouse-wheel-scroll-amount '(4 ((shift) . 4))) ;; one line at a time
-;; (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
-;; (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-;; (pixel-scroll-precision-mode)
 ;;
 ;; (defun dired-up-directory-same-buffer ()
 ;;   "Go up in the same buffer."
@@ -123,23 +104,6 @@
 ;;   (define-key dired-mode-map (kbd "^") 'dired-up-directory-same-buffer))
 ;; (add-hook 'dired-mode-hook #'my-dired-mode-hook)
 ;; (setq dired-use-ls-dired nil)
-
-;; (add-hook 'prog-mode-hook #'display-line-numbers-mode)
-;; (column-number-mode)
-;; Disable visual line mode (this causes issues with $ and a few other things in evil)
-;; (global-visual-line-mode -1)
-;; (setq-default truncate-lines t)
-
-;; Use space to indent by default.
-;; (setq-default indent-tabs-mode nil)
-;; Set appearance of a tab that is represented by 4 spaces.
-;; (setq-default tab-width 2)
-;; (setq-default standard-indent 2)
-;;
-;; (show-paren-mode t)
-;; (setq show-paren-delay 0)
-;; (setq show-paren-style 'parenthesis)
-;; (electric-pair-mode nil)
 
 ;; (setq default-directory "~/code/")
 ;; (setq large-file-warning-threshold nil)
@@ -171,6 +135,23 @@
 (evil-define-key '(normal visual) 'global (kbd "gd") 'lsp-find-definition)
 (evil-define-key '(normal visual) 'global (kbd "gi") 'lsp-find-implementation)
 (evil-define-key '(normal visual) 'global (kbd "gr") 'lsp-find-references)
+
+;; mapping to leader
+;; (map! :leader
+;;       "x" #'your-command-here
+;;       "y" (cmd! (message "Hello world"))
+;;       "f" nil) ; This unbinds what was previously bound to <leader> f
+;; - :leader specifies that the binding should be under the leader key.
+;; - "x" is the key sequence you want to bind after the leader key (e.g., SPC x).
+;; - #'your-command-here is the function or command you want to execute when the key sequence is pressed.
+;; - You can also use (cmd! ...) to define an anonymous command directly within the map! macro.
+;; - Setting a key to nil unbinds any existing binding for that key sequence.
+
+(map! :leader
+      "k" #'kill-buffer)
+
+(map! :leader
+      "<SPC>" #'consult-buffer)
 
 ;; (unless (display-graphic-p)
 ;;   (require 'evil-terminal-cursor-changer)
