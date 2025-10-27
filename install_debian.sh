634d627dd3a101020cb0eaf04df67fe47cc33e61
Willem Boshoff <willem.boshoff@dyna-mo.com>
2025-10-27 11:47:58 +0200

    install scipts

--- install_debian.sh
+++ install_debian.sh
@@ -1,218 +1,54 @@
 #!/usr/bin/env sh
 
-Help() {
-  # Display Help
-  echo "Please choose what to install"
-  echo
-  echo "Syntax: scriptTemplate [-all|terra|emacs|code|docker]"
-  echo "options:"
-  echo "all       All sections."
-  echo "terra     Terraform."
-  echo "emacs     Emacs."
-  echo "code      Visual Studio Code."
-  echo "docker    Docker."
-  echo "pwsh      PowerShell."
-  echo
-}
-
-all=''
-terra=''
-emacs=''
-code=''
-docker=''
-pwsh=''
-
-# Get the options
-while getopts ":hn:" option; do
-  case $option in
-  h) # display Help
-    Help
-    exit
-    ;;
-  all) # Install all
-    all=1 ;;
-  terra) # Install Terraform
-    terra=1 ;;
-  emacs) # Install Emacs
-    emacs=1 ;;
-  code) # Install Visual Studio Code
-    code=1 ;;
-  docker) # Install Docker
-    docker=1 ;;
-  pwsh) # Install PowerShell
-    pwsh=1 ;;
-  \?) # Invalid option
-    echo "Error: Invalid option"
-    exit
-    ;;
-  esac
-done
-
 sudo apt-get update -y
 sudo apt-get dist-upgrade -y
-sudo apt install gpg -y
-sudo apt install wget -y
-sudo apt install -y apt-transport-https
-sudo apt install -y git
-sudo apt install -y ripgrep
-sudo apt install -y bat
-sudo apt install -y curl
-sudo apt install -y exa
-sudo apt install -y gcc
-sudo apt install -y g++
-sudo apt install -y make
-sudo apt install -y autoconf
-sudo apt install -y fd-find
-# sudo apt install -y nodejs
-curl -o- https://fnm.vercel.app/install | bash
-fnm install 22
-sudo apt install -y npm
-sudo apt install -y fontconfig
-sudo apt install -y fzf
-sudo apt install -y tmux
-sudo apt install -y stow
-sudo apt install -y unzip
-sudo apt install -y fira-code-fonts
-sudo apt install -y fonts-firacode
-sudo apt install -y texinfo
-sudo apt install -y libtree-sitter-dev
-sudo apt-get install -y ninja-build gettext cmake build-essential
+
 sudo apt install -y \
-  libfuse2 \
-  libxi6 \
-  libxrender1 \
-  libxtst6 \
-  mesa-utils \
-  libfontconfig \
-  libgtk-3-bin \
-  tar \
-  dbus-user-session
-sudo apt install -y \
-  libgtk-3-dev \
-  libgnutls28-dev \
-  libtiff5-dev \
-  libgif-dev \
-  libjpeg-dev \
-  libpng-dev \
-  libxpm-dev \
-  libncurses-dev
-sudo apt install -y libjansson4 libjansson-dev
-sudo apt install -y \
-  libgccjit0 \
-  libgccjit-13-dev \
-  gcc-13 \
-  g++-13
-sudo apt install -y libmagickcore-dev libmagick++-dev
-
-if [ -n "$code" ] || [ -n "$all" ]; then
-  echo "Installing terraform..."
-  sudo apt install terraform -y
-  wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-  gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com jammy main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-  sudo apt install terraform-ls -y
-else
-  echo "Skipping terraform installation."
-fi
-
-sudo apt update -y
-grep ^Package: /var/lib/apt/lists/apt.releases.hashicorp.com*Packages | sort -u
-sudo apt install luarocks -y
-sudo apt install golang -y
-sudo apt install net-tools -y
-sudo apt install zig -y
-sudo apt install hyperfine -y
-sudo apt install -y git-delta
-
-# linters and formatters
-sudo npm install -g @bufbuild/buf
-sudo npm install -g prettier
-sudo apt install shfmt -y
-
-if [ -n "$docker" ] || [ -n "$all" ]; then
-  echo "Installing pwsh..."
-  # Powershell
-  sudo apt-get update -y
-  sudo apt-get install -y powershell
-  wget -q https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb && sudo dpkg -i packages-microsoft-prod.deb && rm packages-microsoft-prod.deb
-  chsh -s /usr/bin/pwsh
-  # =============================================================================
-else
-  echo "Skipping pwsh installation."
-fi
-
-if [ -n "$docker" ] || [ -n "$all" ]; then
-  echo "Installing Docker..."
-  # Docker
-  # =============================================================================
-  # Add Docker's official GPG key:
-  sudo apt-get install -y ca-certificates curl
-  sudo install -m 0755 -d /etc/apt/keyrings
-  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
-  # Add the repository to Apt sources:
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-      $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
-    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-  sudo apt-get update
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-  sudo apt install -y gnome-terminal
-  wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
-  sudo apt-get install ./docker-desktop-amd64.deb
-  sudo groupadd docker
-  sudo usermod -aG docker $USER
-  # =============================================================================
-else
-  echo "Skipping Docker installation."
-fi
+  git \
+  stow \
+  neovim \
+  nodejs \
+  ripgrep \
+  tmux \
+  fd-find \
+  fzf \
+  zsh \
+  wget \
+  azure-cli \
+  unzip \
+  htop
 
 # Rust
 # =============================================================================
 # Install rustup
+echo "Installing Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # =============================================================================
 
 # use starship shell prompt
 # =============================================================================
+echo "Installing Starship..."
 curl -sS https://starship.rs/install.sh | sh
 stow starship
-sudo apt install -y zsh
+# =============================================================================
+
+# use starship shell prompt
+# =============================================================================
+curl -sS https://starship.rs/install.sh | sh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo lchsh $USER
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # =============================================================================
 
-# nvim and emacs
-# =============================================================================
-mkdir ~/code
-mkdir ~/code/work
-sudo apt install -y neovim
-sudo apt install -y emacs
-# =============================================================================
-
-if [ -n "$code" ] || [ -n "$all" ]; then
-  echo "Installing code..."
-  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
-  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-  echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
-  rm -f packages.microsoft.gpg
-  sudo apt update
-  sudo apt install -y code # or code-insiders
+if [ -d "~/.dotnet" ]; then
+    echo "Installing dotnet..."
+    wget https://dot.net/v1/dotnet-install.sh -O ~/dotnet-install.sh
 else
-  echo "Skipping code installation."
+    echo "Skipping dotnet installation."
 fi
 
-cd ~/.dotfiles
-rm ~/.bashrc
-stow alacritty
-stow bashrc
-stow git
-stow ideavimrc
-stow kitty
-stow nvim
-stow omnisharp
-stow starship
-rm ~/.zshrc
-stow zshrc
+sudo npm install -g @anthropic-ai/claude-code
+curl -sSfL https://get.tur.so/install.sh | bash
+mkdir ~/code
+mkdir ~/code/work
--- install_fedora.sh
+++ install_fedora.sh
@@ -1,91 +1,23 @@
 #!/usr/bin/env sh
 
-Help()
-{
-   # Display Help
-   echo "Please choose what to install"
-   echo
-   echo "Syntax: scriptTemplate [-all|terra|emacs|code|docker]"
-   echo "options:"
-   echo "all       All sections."
-   echo "terra     Terraform."
-   echo "emacs     Emacs."
-   echo "docker    Docker."
-   echo "pwsh      PowerShell."
-   echo
-}
-
-all=''
-terra=''
-emacs=''
-docker=''
-pwsh=''
-
-# Get the options
-while getopts ":hn:" option; do
-   case $option in
-      h) # display Help
-         Help
-         exit;;
-      all) # Install all
-         all=1;;
-      terra) # Install Terraform
-         terra=1;;
-      emacs) # Install Emacs
-         emacs=1;;
-      docker) # Install Docker
-          docker=1;;
-      pwsh) # Install PowerShell
-          pwsh=1;;
-     \?) # Invalid option
-         echo "Error: Invalid option"
-         exit;;
-   esac
-done
-
 sudo dnf update -y
 sudo dnf upgrade -y
-sudo dnf install -y clang cmake ninja-build gtk3-devel
-sudo dnf install -y fd
-sudo dnf install -y shellcheck
-sudo dnf install -y inxi
-sudo dnf install -y stow
 
-git config --global user.email "boshoffwillem@protonmail.com"
-git config --global user.name "Willem Boshoff"
-
-stow ideavimrc
-
-sudo dnf install -y nodejs
-sudo dnf install -y java
-sudo dnf install -y java-devel
-
-# VS Code
-# =============================================================================
-echo "Installing VS Code..."
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf check-update
-sudo dnf install -y code # or code-insiders
-# =============================================================================
-
-# quickemu
-# =============================================================================
-sudo dnf install -y quickemu
-wget https://github.com/quickemu-project/quickgui/releases/download/1.2.10/quickgui-1.2.10+1-linux.rpm
-sudo dnf install -y quickgui-1.2.10+1-linux.rpm
-rm quickgui-1.2.10+1-linux.rpm
-# =============================================================================
-
-# Flutter
-# =============================================================================
-# Install rustup
-echo "Installing Flutter..."
-wget "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.32.7-stable.tar.xz"
-mkdir ~/development
-tar -xf flutter_linux_3.32.7-stable.tar.xz -C ~/development/
-rm flutter_linux_3.32.7-stable.tar.xz
-# =============================================================================
+sudo dnf install -y \
+  git \
+  stow \
+  neovim \
+  nodejs \
+  ripgrep \
+  tmux \
+  fd-find \
+  fzf \
+  zsh \
+  wget \
+  libicu \
+  azure-cli \
+  unzip \
+  htop
 
 # Rust
 # =============================================================================
@@ -101,49 +33,23 @@ curl -sS https://starship.rs/install.sh | sh
 stow starship
 # =============================================================================
 
-if [ -d "~/jetbrains" ]; then
-    echo "Installing jetbrains toolbox..."
-    wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.7.0.48109.tar.gz
-    sudo tar -xvzf jetbrains-toolbox-2.7.0.48109.tar.gz
-    sudo mv jetbrains-toolbox-2.7.0.48109 ~/jetbrains
-    rm jetbrains-toolbox-2.7.0.48109.tar.gz
-else
-    echo "Skipping jetbrains toolbox installation."
-fi
+# use starship shell prompt
+# =============================================================================
+curl -sS https://starship.rs/install.sh | sh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo lchsh $USER
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# =============================================================================
 
 if [ -d "~/.dotnet" ]; then
     echo "Installing dotnet..."
     wget https://dot.net/v1/dotnet-install.sh -O ~/dotnet-install.sh
-    chmod +x ~/dotnet-install.sh
-    ~/dotnet-install.sh -c STS
-    ~/dotnet-install.sh -c LTS
 else
     echo "Skipping dotnet installation."
 fi
 
-if [ -n "$terra" ] || [ -n "$all" ]; then
-    echo "Installing terraform..."
-else
-    echo "Skipping terraform installation."
-fi
-
 sudo npm install -g @anthropic-ai/claude-code
-sudo npm install -g @google/gemini-cli
 curl -sSfL https://get.tur.so/install.sh | bash
-dotnet tool install --ignore-failed-sources --add-source https://api.nuget.org/v3/index.json --global dotnet-ef
-sudo dnf install -y gimp
-sudo dnf install -y inkscape
-# sudo dnf install -y hyprland
-
-sudo dnf install -y neovim
-stow nvim
-
-sudo dnf install -y emacs
-stow emacs
-
 mkdir ~/code
 mkdir ~/code/work
-
-rm ~/.bashrc
-stow bashrc
-source ~/.bashrc
