ab04694c17ad8ee55846b8d96d1d9fd20e25592f
Willem Boshoff <willem.boshoff@dyna-mo.com>
2026-01-08 14:13:54 +0200

    roslyn settings update

--- nvim/.config/nvim/lua/kickstart/lazy.lua
+++ nvim/.config/nvim/lua/kickstart/lazy.lua
@@ -297,7 +297,6 @@ require("lazy").setup({
 
             "csharpier",
             "roslyn",
-            -- "rzls",
 
             "typescript-language-server",
             "eslint-lsp",
@@ -478,6 +477,8 @@ require("lazy").setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 
       local lspconfig = require("lspconfig")
+      local mason_root = require("mason.settings").current.install_root_dir
+      local rzls_path = vim.fn.expand(mason_root .. "/packages/roslyn/libexec/.razorExtension")
 
       local servers = {
         -- clangd = {},
@@ -492,25 +493,6 @@ require("lazy").setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
-        vue_ls = {
-          filetypes = { "vue" },
-          root_dir = lspconfig.util.root_pattern("package.json", "vue.config.js"),
-          settings = {
-            vetur = {
-              completion = {
-                autoImport = true,
-                tagCasing = "kebab",
-                useWorkspaceDependencies = true,
-              },
-              useWorkspaceDependencies = true,
-              validation = {
-                template = true,
-                style = true,
-                script = true,
-              },
-            },
-          },
-        },
 
         fsautocomplete = {},
 
@@ -530,6 +512,16 @@ require("lazy").setup({
         },
 
         roslyn = {
+          cmd = {
+            "roslyn",
+            "--stdio",
+            "--logLevel=Information",
+            "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+            "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
+            "--razorDesignTimePath="
+              .. vim.fs.joinpath(rzls_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
+            "--extension=" .. vim.fs.joinpath(rzls_path, "Microsoft.VisualStudioCode.RazorExtension.dll"),
+          },
           settings = {
             ["csharp|inlay_hints"] = {
               csharp_enable_inlay_hints_for_implicit_object_creation = true,
@@ -579,38 +571,8 @@ require("lazy").setup({
     ---@module 'roslyn.config'
     ---@type RoslynNvimConfig
     ft = { "cs", "razor" },
+    lazy = false,
     opts = {
-      -- "auto" | "roslyn" | "off"
-      --
-      -- - "auto": Does nothing for filewatching, leaving everything as default
-      -- - "roslyn": Turns off neovim filewatching which will make roslyn do the filewatching
-      -- - "off": Hack to turn off all filewatching. (Can be used if you notice performance issues)
-      filewatching = "auto",
-
-      -- Optional function that takes an array of targets as the only argument. Return the target you
-      -- want to use. If it returns `nil`, then it falls back to guessing the target like normal
-      -- Example:
-      --
-      -- choose_target = function(target)
-      --     return vim.iter(target):find(function(item)
-      --         if string.match(item, "Foo.sln") then
-      --             return item
-      --         end
-      --     end)
-      -- end
-      choose_target = nil,
-
-      -- Optional function that takes the selected target as the only argument.
-      -- Returns a boolean of whether it should be ignored to attach to or not
-      --
-      -- I am for example using this to disable a solution with a lot of .NET Framework code on mac
-      -- Example:
-      --
-      -- ignore_target = function(target)
-      --     return string.match(target, "Foo.sln") ~= nil
-      -- end
-      ignore_target = nil,
-
       -- Whether or not to look for solution files in the child of the (root).
       -- Set this to true if you have some projects that are not a child of the
       -- directory with the solution file
@@ -624,67 +586,6 @@ require("lazy").setup({
       -- If the plugin should silence notifications about initialization
       silent = false,
     },
-    -- ADD THIS:
-    -- dependencies = {
-
-    --   {
-    --     -- By loading as a dependencies, we ensure that we are available to set
-    --     -- the handlers for Roslyn.
-    --     "tris203/rzls.nvim",
-    --     config = true,
-    --   },
-    -- },
-    -- lazy = false,
-    -- config = function()
-    --   -- Use one of the methods in the Integration section to compose the command.
-    --   local mason_registry = require("mason-registry")
-    --
-    --   local rzls_path = vim.fn.expand("$MASON/packages/rzls/libexec")
-    --   local cmd = {
-    --     "roslyn",
-    --     "--stdio",
-    --     "--logLevel=Information",
-    --     "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-    --     "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
-    --     "--razorDesignTimePath=" .. vim.fs.joinpath(rzls_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
-    --     "--extension",
-    --     vim.fs.joinpath(rzls_path, "RazorExtension", "Microsoft.VisualStudioCode.RazorExtension.dll"),
-    --   }
-    --
-    --   vim.lsp.config("roslyn", {
-    --     cmd = cmd,
-    --     handlers = require("rzls.roslyn_handlers"),
-    --     settings = {
-    --       ["csharp|inlay_hints"] = {
-    --         csharp_enable_inlay_hints_for_implicit_object_creation = true,
-    --         csharp_enable_inlay_hints_for_implicit_variable_types = true,
-    --         csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-    --         csharp_enable_inlay_hints_for_types = true,
-    --         dotnet_enable_inlay_hints_for_indexer_parameters = true,
-    --         dotnet_enable_inlay_hints_for_literal_parameters = true,
-    --         dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-    --         dotnet_enable_inlay_hints_for_other_parameters = true,
-    --         dotnet_enable_inlay_hints_for_parameters = true,
-    --         dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-    --         dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-    --         dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-    --       },
-    --       ["csharp|code_lens"] = {
-    --         dotnet_enable_references_code_lens = true,
-    --       },
-    --     },
-    --   })
-    --   vim.lsp.enable("roslyn")
-    -- end,
-    -- init = function()
-    --   -- We add the Razor file types before the plugin loads.
-    --   vim.filetype.add({
-    --     extension = {
-    --       razor = "razor",
-    --       cshtml = "razor",
-    --     },
-    --   })
-    -- end,
   },
 
   { -- Autoformat
