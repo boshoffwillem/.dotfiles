eb37e52d01cee52594fcc951581a2e21ecf97e1a
Willem Boshoff <willem.boshoff@dyna-mo.com>
2026-07-16 11:33:02 +0200

    lsp symbols

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -12,7 +12,7 @@ return {
       end,
     },
     { "nvim-telescope/telescope-ui-select.nvim" },
-    { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+    { "nvim-tree/nvim-web-devicons",            enabled = vim.g.have_nerd_font },
   },
   config = function()
     -- [[ Configure Telescope ]]
@@ -43,6 +43,9 @@ return {
     vim.keymap.set({ "n", "v" }, "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
     vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
     vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+    vim.keymap.set("n", "<leader>ls", builtin.lsp_document_symbols, { desc = "[L]SP [S]ymbols" })
+    vim.keymap.set("n", "<leader>lw", builtin.lsp_workspace_symbols, { desc = "[L]SP [W]orkspace Symbols" })
+    vim.keymap.set("n", "<leader>ld", builtin.lsp_dynamic_workspace_symbols, { desc = "[L]SP [D]ynamic Symbols" })
     vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "[O]pen [D]iagnostic" })
     vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
     vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
